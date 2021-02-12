2843378d029a84b1346584f1add076928e6c4832
damian <damian@danielcastelao.org>
2021-02-12 09:57:45 +0100

    importamos libreria

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -1,8 +1,11 @@
 package com.dam1;
 
+import libreriasv8.MisMetodos;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String aux = MisMetodos.saludo("Hi");
+        System.out.println(aux);
     }
 }
