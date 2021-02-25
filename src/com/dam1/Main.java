706261d01bb2b20ab6c715f9794f03f5878760a7
Damian Nogueiras <damiannogueiras@gmail.com>
2021-02-25 22:40:45 +0100

    Utilizamos las constantes de la factoria para diferenciar consola o ventana

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -3,14 +3,15 @@ package com.dam1;
 import libreriasv8.FactoriaDeSalidas;
 import libreriasv8.InterfaceMetodosComunes;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
-        InterfaceMetodosComunes unaConsola = FactoriaDeSalidas.getProducto(1, "Hi");
+        InterfaceMetodosComunes unaConsola = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.CONSOLA, "Hi");
         unaConsola.visualiza();
 
-        InterfaceMetodosComunes unaVentana = FactoriaDeSalidas.getProducto(2,"Bye");
+        InterfaceMetodosComunes unaVentana = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.VENTANA,"Bye");
         unaVentana.visualiza();
     }
 }
