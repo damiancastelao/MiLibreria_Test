726b81300280a88338cdd33f4781e2a2bd568c06
Damian Nogueiras <damian@danielcastelao.org>
2022-02-02 11:33:20 +0100

    Utilizamos una clase de la libreria

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -1,8 +1,13 @@
 package com.dam1;
+// importamos la libreria
+import libreriasv8.Consola;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // iinstanciamos una clase de la libreria
+	    Consola miConsola = new Consola("Hola Consola");
+        // usamos un método del nuevo objeto
+        miConsola.visualiza();
     }
 }
