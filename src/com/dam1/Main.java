5e38bc065e7900bf92826994af4599bc8504115d
Damian Nogueiras <damian@danielcastelao.org>
2022-02-02 11:13:21 +0100

    Añadimos .gitignore

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -1,17 +1,8 @@
 package com.dam1;
 
-import libreriasv8.FactoriaDeSalidas;
-import libreriasv8.InterfaceMetodosComunes;
-
-
 public class Main {
 
     public static void main(String[] args) {
-
-        InterfaceMetodosComunes unaConsola = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.CONSOLA, "Hi");
-        unaConsola.visualiza();
-
-        InterfaceMetodosComunes unaVentana = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.VENTANA,"Bye");
-        unaVentana.visualiza();
+	// write your code here
     }
 }
